cc7a0821365cbcebbf37f8842de1ec8e90a6c4d6
Per Jansson <per.r.jansson@gmail.com>
2014-05-07 21:12:31 +0200

    Added more test

--- src/main/webapp/WEB-INF/test/casper.js
+++ src/main/webapp/WEB-INF/test/casper.js
@@ -4,4 +4,8 @@ casper.start('http://127.0.0.1:8080/report/', function() {
    this.echo(this.getTitle());
 });
 
+casper.thenOpen('http://phantomjs.org', function() {
+    this.echo(this.getTitle());
+});
+
 casper.run();
