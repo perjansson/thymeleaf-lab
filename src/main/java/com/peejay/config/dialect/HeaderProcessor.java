a0f196e24cb423accfda3f4553b35182ac0f10f6
Per Jansson <per.r.jansson@gmail.com>
2014-05-11 01:13:37 +0200

    All the way

--- src/main/java/com/peejay/config/dialect/HeaderProcessor.java
+++ src/main/java/com/peejay/config/dialect/HeaderProcessor.java
@@ -1,8 +1,12 @@
 package com.peejay.config.dialect;
 
 import org.thymeleaf.Arguments;
+import org.thymeleaf.Configuration;
 import org.thymeleaf.dom.Element;
 import org.thymeleaf.processor.attr.AbstractTextChildModifierAttrProcessor;
+import org.thymeleaf.standard.expression.IStandardExpression;
+import org.thymeleaf.standard.expression.IStandardExpressionParser;
+import org.thymeleaf.standard.expression.StandardExpressions;
 
 public class HeaderProcessor extends AbstractTextChildModifierAttrProcessor {
 
@@ -12,7 +16,11 @@ public class HeaderProcessor extends AbstractTextChildModifierAttrProcessor {
 
     @Override
     protected String getText(Arguments arguments, Element element, String attributeName) {
-        return element.getAttributeValue(attributeName);
+        final Configuration configuration = arguments.getConfiguration();
+        final IStandardExpressionParser parser = StandardExpressions.getExpressionParser(configuration);
+        final String attributeValue = element.getAttributeValue(attributeName);
+        final IStandardExpression expression = parser.parseExpression(configuration, arguments, attributeValue);
+        return (String) expression.execute(configuration, arguments);
     }
 
     @Override
