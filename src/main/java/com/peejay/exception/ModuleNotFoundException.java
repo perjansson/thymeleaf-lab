7b2acba577a1f72f939657c1c0e227e8d87c8a0a
Per Jansson <per.r.jansson@gmail.com>
2014-05-26 18:19:59 +0200

    Using styling on 404 page.

--- src/main/java/com/peejay/exception/ModuleNotFoundException.java
+++ src/main/java/com/peejay/exception/ModuleNotFoundException.java
@@ -3,6 +3,6 @@ package com.peejay.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus( value = HttpStatus.NOT_FOUND )
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ModuleNotFoundException extends RuntimeException {
 }
